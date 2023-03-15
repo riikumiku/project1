d64084e3551e065b8b4a4b831e46fd531d235888
lue12 <lue12@192.168.1.44>
2023-03-15 11:20:27 +0530

    FirstCommit

---
+++ src/main/java/com/testproject/TestProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.testproject;
+
+/**
+ *
+ * @author lue12
+ */
+public class TestProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int s = 78;
+        for (int i = 0; i < 70; i++) {
+            if (s == 90) {
+                int count = 0;
+            }
+
+        }
+    }
+}
